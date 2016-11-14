25072dbccb01d35fa72fe145ba33da6011b3a716
miguel <miguel.sanchez528@gmail.com>
2016-11-14 16:06:01 -0300

    first commit

--- src/app/Main.js
+++ src/app/Main.js
@@ -8,14 +8,16 @@ import Dialog from 'material-ui/Dialog';
 import {deepOrange500} from 'material-ui/styles/colors';
 import FlatButton from 'material-ui/FlatButton';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-const styles = {
-  container: {
-    textAlign: 'center',
-    paddingTop: 200,
-  },
-};
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AutoComplete from 'material-ui/AutoComplete';
+
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+
+import TextField from 'material-ui/TextField';
+
+import {Tabs, Tab} from 'material-ui/Tabs';
 
 const muiTheme = getMuiTheme({
   palette: {
@@ -23,58 +25,149 @@ const muiTheme = getMuiTheme({
   },
 });
 
+const styles = {
+  headline: {
+    fontSize: 24,
+    paddingTop: 16,
+    marginBottom: 12,
+    fontWeight: 400,
+  },
+  margentop:{
+    margentop:20
+  },
+  margenbottom:{
+    marginBottom:20
+  }
+};
+
 class Main extends Component {
   constructor(props, context) {
     super(props, context);
-
     this.state = {
       open: false,
+      series:[
+      "54-41690450",
+      "54-41690703",
+      "54-41690704",
+      "54-41690708",
+      "54-41690709",
+      "54-41690711",
+      "54-41690712",
+      "54-41690713",
+      "54-41690714",
+      "54-41690715",
+      "54-41690717",
+      "54-41690718",
+      "54-41772950",
+      "54-41772954",
+      "54-41772957",
+      "54-41772958",
+      "54-41772962",
+      "54-41772963",
+      "54-41772964",
+      "54-41772965",
+      "54-41690721",
+      "54-41690722",
+      "54-41690727",
+      "54-41690730",
+      "54-41697713",
+      "54-41697714",
+      "54-41697715",
+      "54-41697716",
+      "54-41697721",
+      "54-41697726",
+      "54-41697727",
+      "54-41697728",
+      "54-41697731",
+      "54-41697732"
+    ]
     };
   }
 
-  handleRequestClose = () => {
-    this.setState({
-      open: false,
-    });
-  }
-
-  handleTouchTap = () => {
-    this.setState({
-      open: true,
-    });
-  }
-
   render() {
-    const standardActions = (
-      <FlatButton
-        label="Ok"
-        primary={true}
-        onTouchTap={this.handleRequestClose}
-      />
-    );
-
     return (
-      <MuiThemeProvider muiTheme={muiTheme}>
-        <div style={styles.container}>
-          <Dialog
-            open={this.state.open}
-            title="Super Secret Password"
-            actions={standardActions}
-            onRequestClose={this.handleRequestClose}
-          >
-            1-2-3-4-5
-          </Dialog>
-          <h1>Material-UI</h1>
-          <h2>example project</h2>
-          <RaisedButton
-            label="Super Secret Password"
-            secondary={true}
-            onTouchTap={this.handleTouchTap}
-          />
-        </div>
-      </MuiThemeProvider>
+        <MuiThemeProvider>
+          <Tabs>
+            <Tab label="Equipo" style={styles.margenbottom}>
+              <div>
+                <AutoComplete
+                    hintText="Numero de Serie"
+                    dataSource={this.state.series}
+                />
+                <FloatingActionButton mini={true} secondary={true} >
+                  <ContentAdd />
+                </FloatingActionButton>
+              </div>
+              <div>
+                <TextField
+                    hintText="ID2"
+                    floatingLabelText="ID2"
+                    multiLine={true}
+                />
+              </div>
+              <div>
+                <TextField
+                    hintText="ID3"
+                    floatingLabelText="ID3"
+                    multiLine={true}
+                    rows={1}
+                />
+              </div>
+              <div>
+                <TextField
+                    hintText="Address"
+                    floatingLabelText="Address"
+                    multiLine={true}
+                />
+              </div>
+              <div>
+                <TextField
+                    hintText="Address2"
+                    floatingLabelText="Address2"
+                    multiLine={true}
+                />
+              </div>
+            </Tab>
+            <Tab label="Item Two" >
+              <div>
+              </div>
+            </Tab>
+            <Tab
+                label="onActive"
+                data-route="/home"
+            >
+              <div>
+
+              </div>
+            </Tab>
+          </Tabs>
+        </MuiThemeProvider>
     );
   }
 }
 
 export default Main;
+
+/*
+* <div>
+ <MuiThemeProvider muiTheme={muiTheme}>
+ <div>
+ <AutoComplete
+ hintText="Numero de Serie"
+ dataSource={this.state.dataSource}
+ />
+ <FloatingActionButton mini={true} secondary={true} >
+ <ContentAdd />
+ </FloatingActionButton>
+ </div>
+ </MuiThemeProvider>
+ <MuiThemeProvider muiTheme={muiTheme}>
+ <div>
+ <TextField
+ disabled={false}
+ hintText="Disabled Hint Text"
+ />
+ </div>
+ </MuiThemeProvider>
+ </div>
+* */
